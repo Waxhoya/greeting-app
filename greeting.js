d790ae378f85627837fb7afeac0a41500c8bf2fe
Waxhoya <waxhoya@gmail.com>
2016-10-18 14:24:03 -0700

    Node code works. Testing works.

--- greeting.js
+++ greeting.js
@@ -1,4 +1,7 @@
 
+var rl = require('readline');
+var prompts = rl.createInterface(process.stdin, process.stdout);
+
 module.exports = {
     greeting: function(userName){
             name = userName || 'Tim';
@@ -8,6 +11,13 @@ module.exports = {
     makeFarewell: function(userName){
             name = userName || 'Tim';
             return 'Hello ' + name;
-        }
+        },
+    consoleUser: function() { prompts.question('What is your name?', function(nameInput) {
+        name = nameInput || 'Tim';
+        return 'Hello ' + name;
+        process.exit();
+        })}
+
 };
 
+module.exports.consoleUser();
--- test.js
+++ test.js
@@ -4,12 +4,12 @@ var assert = require('assert'); // allows easier comparison of tests.
 describe('Testing greeting function', function() {
 
     it('Greeted user', function() {
-        var greetTest = welcome('Chuck');
+        var greetTest = welcome.greeting('Chuck');
         assert.equal( greetTest, 'Hello Chuck' );
     });
 
     it('Hello Tim if name not given', function() {
-        var greeting = welcome();
+        var greeting = welcome.greeting();
         assert.equal( greeting, 'Hello Tim' );
     });
 }); 
